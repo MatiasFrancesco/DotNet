d241fc2f7d01b331810b5df7aaaaa9f851619244
DrunkFox <matiasfrancesco04@gmail.com>
2023-09-15 23:52:10 +0200

    created a Layout View

---
+++ MyCourse/Views/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCourse.Views.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
