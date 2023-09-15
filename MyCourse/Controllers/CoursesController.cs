dc803bceb28f233e2dfc3ff99307e0102319abc5
DrunkFox <matiasfrancesco04@gmail.com>
2023-09-15 22:27:28 +0200

    created simple Asp.Net Core MVC

---
+++ MyCourse/Controllers/CoursesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index() => Content("I'm Index");
+        public IActionResult Detail(string id) => Content($"I'm Detail, I have recieve the id = {id}");
+    }
+}
---
+++ MyCourse/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+
+
+
+//  /courses/detail/5
+//app.UseMvcWithDefaultRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
+
+
+
+
+
+
+
+
+
+
+
